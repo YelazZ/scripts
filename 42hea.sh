afd8d80943b4468fabfcea9737f57c2ebe63f1c5
YelazZ <bigsef94@gmail.com>
2019-05-23 20:56:00 +0000

    adding 42 header script for vim

---
+++ 42hea.sh
@@ -0,0 +1,12 @@
+cd 
+mkdir .vim
+cd .vim
+mkdir plugin
+cd plugin
+git clone https://github.com/gcamerli/42header.git
+cd 42header
+chmod +x set_header.sh
+./set_header.sh
+cd ..
+rm -rf 42header
+
